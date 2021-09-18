2ed0518b8c42334a01b4b7fcc543e87f927886e0
Sneha Baser <61595935+sneha-baser@users.noreply.github.com>
2021-09-18 14:31:35 +0530

    Create MaximumAndMinimum.cpp

---
+++ MaximumAndMinimum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+int n;
+cin>>n;
+int i,a[n];
+for(i=0;i<n;i++)
+cin>>a[i];
+int maxx,minn;
+if(n==1)
+{
+    maxx=a[0];
+    minn=a[0];
+}
+else {
+    if(a[0]>a[1]){
+        maxx=a[0];
+        minn=a[1];
+    }
+    else{
+        maxx=a[1];
+        minn=a[0]; 
+    }
+    for(i=2;i<n;i++){
+      if(a[i]>maxx){
+        maxx=a[i];
+      }
+    else if(a[i]<minn){
+        minn=a[i];
+     }
+   }
+}
+cout<<maxx<<" "<<minn<<endl;
+	return 0;
+}
