8a4ba427a3572da4e8040205a3179984d373f2be
Sneha Baser <61595935+sneha-baser@users.noreply.github.com>
2021-09-18 14:15:44 +0530

    Create ReverseArray.cpp

---
+++ ReverseArray.cpp
@@ -0,0 +1,13 @@
+string reverseWord(string str){
+    
+  //Your code here
+  int l=str.length(),i,j;
+  i=0;
+  j=l-1;
+  while(i<j){
+      swap(str[i],str[j]);
+      i++;
+      j--;
+  }
+  return str;
+}
